07c5a55cd92e7529670ce1b832f9c0d68b739268
Toby Dragon <tdragon@ithaca.edu>
2018-12-06 11:45:06 -0500

    cleaned up and made realistically useful

--- ParallelizedImageTimeComparison.py
+++ ParallelizedImageTimeComparison.py
@@ -2,56 +2,54 @@ import copy
 from time import *
 from readwritePPM import *
 from multiprocessing import Pool
+import timeit
 
 
-def reduceColor(info):
-    image=info[0]
-    factor=info[1]
-    name=info[2]
-    imOut=info[3]
+def reduceColor(args):
+    imageIn = args[0]
+    factor = args[1]
+    imageOut = args[2]
     print("Starting\t", factor)
     start = time()
-    # make it happen 5 times to increase task time
-    for a in range(5):
-        for y in range(len(image)):
-            row=[]
-            for x in range(len(image[y])):
-                pixel=[]
-                for c in range(len(image[y][x])):
-                    pixel.append(int(image[y][x][c]/factor)*factor)
-                row.append(pixel)
-            imOut.append(row)
+    for y in range(len(imageIn)):
+        row=[]
+        for x in range(len(imageIn[y])):
+            pixel=[]
+            for c in range(len(imageIn[y][x])):
+                pixel.append(int(imageIn[y][x][c]/factor)*factor)
+            row.append(pixel)
+            imageOut.append(row)
     print("Ending\t\t", factor, " time:", str(time()-start))
+    # return imOut
 
 
 def main():
 
     # set up list of lists of arguments for the reduceColor function
-    im=readPPM("images/dog.ppm")
+    imageData=readPPM("images/dog.ppm")
     argsIn=[]
     for x in range(1,4):
-        factor=2**x
-        name="reduceColor{:03}.ppm".format(factor)
-        temp=[im,factor,name, []]
+        factor = 2**x
+        temp = imageData, factor, []
         argsIn.append(temp)
 
     parallelArgsIn = copy.deepcopy(argsIn)
 
     # single thread
-    start=time()
+    start = time()
     for x in range(len(argsIn)):
         reduceColor(argsIn[x])
     finish=time()
     print("Reduce color single thread: "+str(finish-start)+" seconds")
 
     #multi thread
-
-    pool=Pool(3)
-    start=time()
+    pool = Pool(3)
+    start = time()
     pool.map(reduceColor, parallelArgsIn)
-    finish=time()
+    finish = time()
     pool.close()
 
     print("Reduce color multi thread: "+str(finish-start)+" seconds")
 
 main()
+
